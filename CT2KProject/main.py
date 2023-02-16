c90b89da2f75ff233260d1fb855caa96167b686b
jorgesiverio <jspd8404@gmail.com>
2023-02-15 23:27:33 -0700

    Added argparse block

--- CT2KProject/main.py
+++ CT2KProject/main.py
@@ -1,5 +1,29 @@
 import PyPDF2
 import re
+import os
+import argparse
+
+import argparse as argparse
+
+parser = argparse.ArgumentParser(description="Extract identifiers from cybertips and generats keyword list")
+parser.add_argument("-s", "--source", metavar="root", type=str, help="Directory where cybertips are located")
+parser.add_argument("-o", "--output", metavar="outputDirectory", type=str, help="Keyword file destination")
+parser.add_argument("-a", "--all", action="store_true", help="Extracts all identifiers")
+parser.add_argument("-c", "--combined", action="store_true", help= "Puts all identifiers in one file")
+parser.add_argument("-e", "--separate", action="store_true", help="Separates identifiers per category")
+parser.add_argument("-u", "--usernames", action="store_true", help="Extracts usernames and/or ESP user ID only")
+parser.add_argument("-f", "--files", action="store_true", help="Extracts file names only")
+parser.add_argument("-ha", "--hashes", action="store_true", help="Extracts hash values only")
+parser.add_argument("-i", "--ip", action="store_true", help="Extracts all IPs")
+args = parser.parse_args()
+#print(args.echo)
+
+def cybertips(root):
+    for root, dirs, files in os.walk(".", topdown=False):
+        for name in files:
+            print(os.path.join(root, name))
+        for name in dirs:
+            print(os.path.join(root, name))
 
 
 # Return page where keyword hit is found.
@@ -106,6 +130,9 @@ def get_ip(pdfstream, start_page):
     ip_list = list(dict.fromkeys(ip_list))
     print(len(ip_list))
     return ip_list
+
+#Main
+
 
 
 
